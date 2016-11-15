191ee2dd0addf8b66a3d61269f64621c9d5c38f5
ruelle <ruelle@ruelle-System-Product-Name.(none)>
2016-11-15 23:05:28 +0100

    mkreal added

---
+++ fetch.h
@@ -0,0 +1,7 @@
+#ifndef FETCH_H
+#define FETCH_H
+
+int is_pending( int fd );
+int fetch_pending( int fd );
+
+#endif
---
+++ mkreal.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include "fetch.h"
+
+int main( int argc, char* const argv[] ) {
+	int fd;
+
+	if( argc < 2 ) {
+		printf( "Usage: %s filename\n", argv[0] );
+		return 0;
+	}
+
+	fd = open( argv[1], O_RDWR );
+	if( fd == -1 ) {
+		perror( "" );
+		return 1;
+	}
+	if( fetch_pending( fd ) == -1 )
+		return 1;
+
+	return 0;
+}
